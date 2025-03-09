bb8f3af8d5d19998b96d144e390e8b57c7c094f0
xryptt <xryptt@gmail.com>
2025-03-09 14:14:58 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Hello Railway!'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
